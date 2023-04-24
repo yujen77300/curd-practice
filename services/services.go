cdc098d8fffca3869dcb7ac342fa71a243eae68b
yujen77300 <yujenhuang24@gmail.com>
2023-04-24 11:59:09 +0800

    feat:change data stored in storage to database

--- services/services.go
+++ services/services.go
@@ -3,86 +3,95 @@ package services
 import (
 	"errors"
 	"time"
+
 	"github.com/google/uuid"
+	"github.com/yujen77300/curd-practice/database"
 	"github.com/yujen77300/curd-practice/models"
 )
 
-//  storage is created to store the item’s data.
-var storage []models.Item = []models.Item{}
-
-
 func GetAllItems() []models.Item {
-		return storage
+	// create a variable to store items data
+	var items []models.Item = []models.Item{}
+
+	// get all data from the database order by created_at
+	database.DB.Order("created_at desc").Find(&items)
+
+	// return all items from the database
+	return items
 }
 
 // GetItemByID returns get item's data by ID
-func GetItemByID(id string) (models.Item, error){
-	 for _,item := range storage{
-		 if item.ID == id{
-			 return item,nil
-		 }
-	 }
-	 return models.Item{}, errors.New("item not found")
+func GetItemByID(id string) (models.Item, error) {
+	// create a variable to store item data
+	var item models.Item
+
+	// get item data from the database by ID
+	result := database.DB.First(&item, "id = ?", id)
+
+	// if the item data is not found, return an error
+	if result.RowsAffected == 0 {
+		return models.Item{}, errors.New("item not found")
+	}
+
+	// return the item data from the database
+	return item, nil
 }
 
 // CreateItem returns created item in the storage
 func CreateItem(itemRequest models.ItemRequest) models.Item {
-    // create a new item
-    var newItem models.Item = models.Item{
-        ID:        uuid.New().String(),
-        Name:      itemRequest.Name,
-        Price:     itemRequest.Price,
-        Quantity:  itemRequest.Quantity,
-        CreatedAt: time.Now(),
-    }
+	// create a new item
+	var newItem models.Item = models.Item{
+		ID:        uuid.New().String(),
+		Name:      itemRequest.Name,
+		Price:     itemRequest.Price,
+		Quantity:  itemRequest.Quantity,
+		CreatedAt: time.Now(),
+	}
 
-    // store the created item into storage
-    storage = append(storage, newItem)
+	// insert the new item data into the database
+	database.DB.Create(&newItem)
 
-    // return the item that already created
-    return newItem
+	// return the item that already created
+	return newItem
 }
 
 // UpdateItem returns updated item
 func UpdateItem(itemRequest models.ItemRequest, id string) (models.Item, error) {
-    // iterate through all items
-    for index, item := range storage {
-        // if item is found
-        if item.ID == id {
-            // update the item's data
-            item.Name = itemRequest.Name
-            item.Price = itemRequest.Price
-            item.Quantity = itemRequest.Quantity
-            item.UpdatedAt = time.Now()
+	// get the item data by ID
+	item, err := GetItemByID(id)
 
-            storage[index] = item
+	// if item is not found, return an error
+	if err != nil {
+		return models.Item{}, err
+	}
 
-            // return the updated item
-            return item, nil
-        }
-    }
+	// update item data
+	item.Name = itemRequest.Name
+	item.Price = itemRequest.Price
+	item.Quantity = itemRequest.Quantity
+	item.UpdatedAt = time.Now()
 
-    // return error if update is failed
-    return models.Item{}, errors.New("item update failed, item not found")
+	// update the item data in the database
+	database.DB.Save(&item)
+
+	// return the updated item
+	return item, nil
 }
 
 // DeleteItem returns deletion result
 func DeleteItem(id string) bool {
-    // create a new slice for storing items data
-    // after deletion
-    var newItems []models.Item = []models.Item{}
+	// get the item data by ID
+	item, err := GetItemByID(id)
 
-    // iterate through all items
-    for _, item := range storage {
-        // if current item's ID is not equal ID in parameter
-        // insert the item into "newItems" slice
-        if item.ID != id {
-            newItems = append(newItems, item)
-        }
-    }
+	// if item is not found, return false
+	if err != nil {
+		return false
+	}
 
-    // assign the "newItems" into storage
-    storage = newItems
+	// delete the item data
+	database.DB.Delete(&item)
 
-    return true
+	// return true
+	// this means the deletion is succeed
+	return true
 }
